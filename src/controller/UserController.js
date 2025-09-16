33a94cff4832c0263359dc0bb684b28157089e27
dieutuyetnguyenthi <dieutuyetnguyenthi@gmail.com>
2025-09-17 00:17:48 +0700

    no test

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -3,7 +3,6 @@ const userController = {
   // POST /helloworld
   create: (req, res) => {
     const { id, email } = req.currentUser;
-    const{}
     res.status(200).json({
         message: "xinchao: " + id + " email: " + email,
         timestamp: new Date().toISOString(),
