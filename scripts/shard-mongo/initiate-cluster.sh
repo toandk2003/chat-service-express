6f9cd5f11fa7b1d34ad5ef71f554557e768205e1
DamianDuy21 <damianduy21@gmail.com>
2025-09-18 21:54:18 +0700

    fix script

--- scripts/shard-mongo/initiate-cluster.sh
+++ scripts/shard-mongo/initiate-cluster.sh
@@ -14,36 +14,38 @@ rs.initiate({
 "
 
 # Step 2  : init shard1 replicaSet > then check using docker exec -it shard1_node1 mongosh --eval "rs.status()"
-docker exec -it shard1_node1 mongosh --port 27017 --eval '
+docker exec -it shard1_node1 mongosh --port 27017 --eval "
 rs.initiate({
-  _id: "shard1",
+  _id: 'shard1',
   members: [
-    { _id: 0, host: "shard1_node1:27017" },
-    { _id: 1, host: "shard1_node2:27017" },
-    { _id: 2, host: "shard1_node3:27017" }
+    { _id: 0, host: 'shard1_node1:27017' },
+    { _id: 1, host: 'shard1_node2:27017' },
+    { _id: 2, host: 'shard1_node3:27017' }
   ]
 })
-'
+"
+
 
 # Step 3  : init shard2 replicaSet > then check using docker exec -it shard2_node1 mongosh --eval "rs.status()"
-docker exec -it shard2_node1 mongosh --port 27017 --eval '
+docker exec -it shard2_node1 mongosh --port 27017 --eval "
 rs.initiate({
-  _id: "shard2",
+  _id: 'shard2',
   members: [
-    { _id: 0, host: "shard2_node1:27017" },
-    { _id: 1, host: "shard2_node2:27017" },
-    { _id: 2, host: "shard2_node3:27017" }
+    { _id: 0, host: 'shard2_node1:27017' },
+    { _id: 1, host: 'shard2_node2:27017' },
+    { _id: 2, host: 'shard2_node3:27017' }
   ]
 })
-'
+"
 
-# Step 3 Add shard
- docker exec -it mongos mongosh --port 27017 --eval "
+
+# Step 4 Add shard
+docker exec -it mongos mongosh --port 27017 --eval "
 sh.addShard('shard1/shard1_node1:27017,shard1_node2:27017,shard1_node3:27017')
 sh.addShard('shard2/shard2_node1:27017,shard2_node2:27017,shard2_node3:27017')
 "
 
-# Step 4 enable sharding
+# Step 5 enable sharding
 docker exec -it mongos mongosh --port 27017 --eval "
 // Enable sharding for chat_service database
 sh.enableSharding('chat_service')
