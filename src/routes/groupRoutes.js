93fd7e543c4037667dbc24721562d5d12203a2d0
dieutuyetnguyenthi <dieutuyetnguyenthi@gmail.com>
2025-11-02 22:41:23 +0700

    Leave group

--- src/routes/groupRoutes.js
+++ src/routes/groupRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const createGroup = require("../services/createGroup");
 const addMemberToGroup = require("../services/addMemberToGroup");
 const removeMemberToGroup = require("../services/removeMemberToGroup copy");
+const leaveGroup = require("../services/leaveGroup");
 
 const groupRoutes = express.Router();
 groupRoutes.post("/groups", createGroup);
 groupRoutes.put("/groups/:id/add-member", addMemberToGroup);
 groupRoutes.delete("/groups/:id/delete-member", removeMemberToGroup);
+groupRoutes.delete("/groups/:id/leave-group", leaveGroup);
 
 module.exports = groupRoutes;
---
+++ src/services/leaveGroup.js
@@ -0,0 +1,133 @@
+const { User } = require("../models/User");
+const { Message } = require("../models/Message");
+const Conversation = require("../models/Conversation");
+const mongoose = require("mongoose");
+const withTransactionThrow = require("../common/utils/withTransactionThrow");
+const {
+  getMyConversationByUserIdAndConversationId,
+} = require("./getMyConversation");
+// const convertUserToLongFormat = require("../common/utils/convertUserToLongFormat");
+const leaveGroup = async (req, res) => {
+  return await withTransactionThrow(
+    async (session, req, res) => {
+      console.log("\nstart-create-group\n"); // In ra console server
+      const { isKeyMember = false, newKeyMemberId = null } = req.body;
+
+      const { id } = req.params;
+      const conversationId = new mongoose.Types.ObjectId(id);
+
+      const email = req.currentUser.email;
+      console.log("email: " + email);
+      const user = await User.findOne({ email, status: "ACTIVE" });
+      console.log("user: ", user);
+      const userId = user._id;
+
+      const [ourConversation, myConversation] =
+        await getMyConversationByUserIdAndConversationId(
+          userId,
+          conversationId
+        );
+
+      if (!ourConversation || !myConversation)
+        throw new Error("CONVERSATION NOT FOUND WITH ID: " + conversationId);
+
+      if (!ourConversation.leaderId.equals(userId)) {
+        throw new Error(
+          "YOU ARE NOT LEADER OF CONVERSATION WITH ID: " + conversationId
+        );
+      }
+
+      ourConversation.currentMember -= 1;
+
+      ourConversation.participants = ourConversation.participants.filter(
+        (participant) => !participant.userId.equals(userId)
+      );
+      if (isKeyMember) {
+        if (!newKeyMemberId) throw new Error("newKeyMemberId must not be null");
+        ourConversation.leaderId = newKeyMemberId;
+      }
+
+      const skipUntilOffset = myConversation.skipUntilOffset;
+      let messages = [];
+
+      if (myConversation.status === "running") {
+        messages = await Message.find({
+          _id: {
+            $gt: skipUntilOffset,
+          },
+          conversationId,
+          status: "CONFIRMED",
+        }).sort({ createdAt: -1 }); // Sắp xếp giảm dần theo thời gian tạo
+      }
+      console.log("messages: ", messages);
+
+      //update lastReadOffset
+      if (messages.length > 0) {
+        myConversation.lastReadOffset = messages[0]._id;
+      }
+
+      await ourConversation.save({ session });
+
+      user.conversations.push({ _id: ourConversation._id });
+      user.conversations = user.conversations.filter(
+        (conversation) => !conversation._id.equals(ourConversation._id)
+      );
+
+      await user.save({ session });
+
+      return res.json({
+        success: true,
+        status: 200,
+        message: "Here is your detail conversation",
+        data: {
+          conversation: {
+            _id: ourConversation._id,
+            name: myConversation.view.name,
+            status: myConversation.status,
+            type: ourConversation.type,
+            currentMember: ourConversation.currentMember,
+            maxMember: ourConversation.maxMember,
+            type: ourConversation.type,
+            createdAt: ourConversation.createdAt,
+            updatedAt: ourConversation.updatedAt,
+            lastMessage: null,
+            __v: myConversation.__v,
+            settings: {
+              _id: null,
+              userId: user._id,
+              conversationId: ourConversation._id,
+              getNotifications: myConversation.isReceiveNoti,
+              isPinned: false,
+              language: myConversation.language,
+              translatedTo: myConversation.translatedTo,
+              createdAt: ourConversation.createdAt,
+              updatedAt: ourConversation.updatedAt,
+              __v: ourConversation.__v,
+            },
+            seenBy: ourConversation.participants.map((participant) => {
+              return {
+                userId: participant.userId,
+                messageId: participant.lastReadOffset,
+              };
+            }),
+          },
+          isNewCreated: false,
+          messages: [],
+          user: {
+            _id: userId,
+          },
+          pagination: {
+            currentPage: 0,
+            pageSize: 10,
+            totalItems: 0,
+            totalPages: 0,
+          },
+        },
+      });
+    },
+    req,
+    res
+  );
+};
+
+module.exports = leaveGroup;
