a1fefee0d71259f636d04089254e73c1d87aa69c
dieutuyetnguyenthi <dieutuyetnguyenthi@gmail.com>
2025-11-09 21:21:46 +0700

    Fix two pp

--- src/handlers/socket-event-bus/handleSendNotiReceiveFRForMuti.js
+++ src/handlers/socket-event-bus/handleSendNotiReceiveFRForMuti.js
@@ -3,12 +3,25 @@ const { User } = require("../../models/User");
 const handleSendNotiReceiveFRForMuti = async (message) => {
   console.log("emit  receiveFriendRequest.....");
   console.log("message: " + JSON.stringify(message, null, 2));
+
+  const senderEmail = message.data.sender.email;
   const receiverEmail = message.data.receiver.email;
+
+  console.log("senderEmail: ", senderEmail);
   console.log("receiverEmail: ", receiverEmail);
-  const user = await User.findOne({
-    email: receiverEmail,
-  });
-  const userId = user._id;
-  global.io.to(userId.toString()).emit("receiveFriendRequest", message);
+
+  const [sender, receiver] = await Promise.all(
+    User.findOne({
+      email: senderEmail,
+    }),
+    User.findOne({
+      email: receiverEmail,
+    })
+  );
+  const senderId = sender._id;
+  const receiverId = receiver._id;
+  global.io.to(senderId.toString()).emit("receiveFriendRequest", message);
+  global.io.to(receiverId.toString()).emit("receiveFriendRequest", message);
+  console.log("emit  receiveFriendRequest success.....");
 };
 module.exports = handleSendNotiReceiveFRForMuti;
