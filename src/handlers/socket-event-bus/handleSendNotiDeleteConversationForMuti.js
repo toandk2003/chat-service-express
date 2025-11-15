69c3faee71851710edd929cd7e3e69fe9a0e54ef
dieutuyetnguyenthi <dieutuyetnguyenthi@gmail.com>
2025-11-15 23:02:41 +0700

    Add emit delete conversation

--- src/handlers/socket-event-bus/handleSendNotiDeleteConversationForMuti.js
+++ src/handlers/socket-event-bus/handleSendNotiDeleteConversationForMuti.js
@@ -2,13 +2,19 @@ const handleSendNotiDeleteConversationForMuti = async (message) => {
   console.log("emit delete conver  .....");
   console.log("message: " + JSON.stringify(message, null, 2));
 
-  message.data.actionUserId.forEach((userId) => {
-    global.io.to(userId.toString()).emit("deleteConversation", message);
-  });
+  if (message.data.type === "private" || message.data.type === "bot") {
+    message.data.actionUserId.forEach((userId) => {
+      global.io.to(userId.toString()).emit("deleteConversation", message);
+    });
+  } else if (message.data.type === "group") {
+    message.data.actionUserId.forEach((userId) => {
+      global.io.to(userId.toString()).emit("deleteConversation", message);
+    });
 
-  message.data.remainMemberIds.forEach((userId) => {
-    global.io.to(userId.toString()).emit("deleteConversation_updateMemberList", message);
-  });
+    message.data.remainMemberIds.forEach((userId) => {
+      global.io.to(userId.toString()).emit("deleteConversation", message);
+    });
+  }
 
   console.log("emit delete-conver success.....");
 };
